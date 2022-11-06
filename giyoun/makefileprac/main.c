dcdc84c38f17e3b2973f6ce89bee6661e5d8a187
Ubuntu <ubuntu@ip-172-31-6-242.ap-northeast-2.compute.internal>
2022-11-06 04:44:47 +0000

    makefile study

---
+++ giyoun/makefileprac/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(){
+    printf("Hello, I'm main()\n");
+
+    proc_kor();
+    proc_usa();
+
+    return 0;
+}
---
+++ giyoun/makefileprac/usa.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+void proc_usa(){
+    printf("Hello, I'm USA()\n");
+}
